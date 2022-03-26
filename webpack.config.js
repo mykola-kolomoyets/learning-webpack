df661e639a08b8d1203ec5034d4a7657bda2e304
oleholei <kolomoyets473@gmail.com>
2022-03-26 18:51:23 +0200

    added scss support

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,12 @@ module.exports = {
                     'style-loader', 'css-loader'
                 ]
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
         ]
     }
 };
